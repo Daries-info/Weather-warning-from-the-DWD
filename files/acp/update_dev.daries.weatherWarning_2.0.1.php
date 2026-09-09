9e2dde1f482a63f0211fa1135fbf9d724b2f7ebf
xDeraiser <info@daries.info>
2026-09-09 19:56:27 +0200

    Delete obsolete pre-2.0.1 registry values on update
    Before 2.0.1, warnings, maps and fire indices were stored via
    RegistryHandler under dev.daries.weatherWarning. Since that storage was
    replaced by WeatherWarningCache, those fields are no longer read - but
    were also never removed, so the base64-encoded map images stay in
    wcf1_registry indefinitely on every installation that upgrades.

--- files/acp/update_dev.daries.weatherWarning_2.0.1.php
+++ files/acp/update_dev.daries.weatherWarning_2.0.1.php
@@ -1,7 +1,35 @@
 <?php
 
+use wcf\system\registry\RegistryHandler;
+
 /**
  * @author  Marco Daries, Alexander Langer (Source of ideas)
  * @copyright   2020-2024 Daries.dev
  * @license Daries.info - Free License <https://daries.info/license/free.html>
  */
+
+// Remove the values written by the old RegistryHandler-based storage (< 2.0.1);
+// the data now lives exclusively in the WeatherWarningCache.
+$packageName = 'dev.daries.weatherWarning';
+
+$registryFields = [
+    'weatherWarning',
+    'weatherWarningTime',
+    'forestFireHazardIndexWBI',
+    'grasslandFireIndex',
+    'germanyMap_blackIce',
+    'germanyMap_frost',
+    'germanyMap_fog',
+    'germanyMap_heat',
+    'germanyMap_map',
+    'germanyMap_rain',
+    'germanyMap_snow',
+    'germanyMap_storm',
+    'germanyMap_thaw',
+    'germanyMap_thunder',
+    'germanyMap_uv',
+];
+
+foreach ($registryFields as $field) {
+    RegistryHandler::getInstance()->delete($packageName, $field);
+}
